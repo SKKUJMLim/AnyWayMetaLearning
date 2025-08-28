212ac6f5748693cc04c983c516d3a4caed1c84c6
SKKUJMLim <skku.jmlim@gmail.com>
2025-08-28 15:51:22 +0900

    init

---
+++ train_maml_system.py
@@ -0,0 +1,22 @@
+from data import MetaLearningSystemDataLoader
+from experiment_builder import ExperimentBuilder
+from few_shot_learning_system import MAMLFewShotClassifier
+from utils.parser_utils import get_args
+from utils.dataset_tools import maybe_unzip_dataset
+from multiprocessing import freeze_support
+
+if __name__ == '__main__':
+
+    # python train_maml_system.py --name_of_args_json_file experiment_config/Any_Way_MAML_miniImagenet.json --gpu_to_use 0
+
+    freeze_support()
+
+    # Combines the arguments, model, data and experiment builders to run an experiment
+    args, device = get_args()
+    model = MAMLFewShotClassifier(args=args, device=device,
+                                  im_shape=(2, args.image_channels,
+                                            args.image_height, args.image_width))
+    maybe_unzip_dataset(args=args)
+    data = MetaLearningSystemDataLoader
+    maml_system = ExperimentBuilder(model=model, data=data, args=args, device=device)
+    maml_system.run_experiment()
